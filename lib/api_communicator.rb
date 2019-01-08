04f726c44699794004d1fd5c6442b74f48a8af41
github username <github email address>
2019-01-08 15:25:20 +0000

    finished intial api code

--- lib/api_communicator.rb
+++ lib/api_communicator.rb
@@ -4,9 +4,24 @@ require 'pry'
 
 def get_character_movies_from_api(character_name)
   #make the web request
-  response_string = RestClient.get('http://www.swapi.co/api/people/')
+
+
+  response_string = RestClient.get("http://www.swapi.co/api/people/?search=#{character_name}")
   response_hash = JSON.parse(response_string)
 
+if response_hash["results"][0]
+    character_films = []
+    match = response_hash["results"][0]
+    match["films"].each do |film|
+      film_response_string = RestClient.get(film)
+      film_response_hash = JSON.parse(film_response_string)
+      character_films << film_response_hash
+    end
+  else
+    puts "We do not have a character matching #{character_name}"
+  end
+  character_films
+end
   # iterate over the response hash to find the collection of `films` for the given
   #   `character`
   # collect those film API urls, make a web request to each URL to get the info
@@ -16,16 +31,27 @@ def get_character_movies_from_api(character_name)
   # this collection will be the argument given to `print_movies`
   #  and that method will do some nice presentation stuff like puts out a list
   #  of movies by title. Have a play around with the puts with other info about a given film.
-end
+
 
 def print_movies(films)
   # some iteration magic and puts out the movies in a nice list
+  films.each do |film|
+  puts "------------------------------------------------------"
+  puts "Title: #{film["title"]}"
+  puts "Episode: #{film["episode_id"]}"
+  puts "Director: #{film["director"]}"
+  puts "Producer(s): #{film["producer"]}"
+  puts "Release Date: #{film["release_date"]}"
+  # puts "Opening Crawl: #{film["opening_crawl"][0..100]}" + "..."
+  end
 end
 
 def show_character_movies(character)
   films = get_character_movies_from_api(character)
   print_movies(films)
 end
+
+# print_movies(get_character_movies_from_api("Luke"))
 
 ## BONUS
 
